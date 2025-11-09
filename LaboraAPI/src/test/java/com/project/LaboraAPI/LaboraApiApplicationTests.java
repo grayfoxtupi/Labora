4557af90de1f1354a2d976602c4d7b24c7731410
grayfoxtupi <raulrpn.nunes@proton.me>
2025-11-09 15:25:21 -0300

    Creating api and app

---
+++ LaboraAPI/src/test/java/com/project/LaboraAPI/LaboraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.LaboraAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
